5725ac47dac0e4a0e373ea486548b2c04b77581f
Beckett Hayes <becketthayes@gmail.com>
2025-02-07 10:32:41 -0800

    modified image loading, prepared images for CNN

--- CNN_ocular_disease.py
+++ CNN_ocular_disease.py
@@ -3,17 +3,17 @@ import cv2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+from sklearn.preprocessing import LabelEncoder
 
 csv_path = "full_df.csv"
 df = pd.read_csv(csv_path)
 
 # print(df[["Right-Diagnostic Keywords", "labels"]])
-images_right = []
-labels_right = []
+images = []
+labels = []
 
-images_left = []
-labels_left = []
 
 for index, row in df.iterrows():
 
@@ -21,28 +21,46 @@ for index, row in df.iterrows():
 
     if os.path.exists(path):
         img_info = cv2.imread(path)
-        img_info = cv2.resize(img_info, (224, 224))
+        img_info = cv2.resize(img_info, (256, 256))
         img_info = img_info / 255.0
+        img_info = img_info.astype(np.float32)  # Make sure images are float32
 
-        if "right" in row["filename"]:
-            images_right.append(img_info)
+        images.append(img_info)
 
-            label_right = row["labels"][2]
-            labels_right.append(label_right)
-        else:
-            images_left.append(img_info)
+        label = row["labels"][2]
+        labels.append(label)
 
-            label_left = row["labels"][2]
-            labels_left.append(label_left)
+images = np.array(images)
+labels = np.array(labels)
 
-# tests to check that the arrays were filled correctly
-print(len(images_right), len(labels_right))
-print(len(images_left), len(labels_left))
-print(labels_left)
+label_encoder = LabelEncoder()
+labels = label_encoder.fit_transform(labels)
+print(labels)
+
+dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+
+dataset = dataset.shuffle(buffer_size=1000).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+
+# testing to see if the images and labels are of proper shape
+for batch_images, batch_labels in dataset.take(1):  # take(1) to get just one batch
+    print(f"Batch images shape: {batch_images.shape}")
+    print(f"Batch labels shape: {batch_labels.shape}")
+    print(f"Batch images dtype: {batch_images.dtype}")
+    print(f"Batch labels dtype: {batch_labels.dtype}")
+
+    # test to see if image is loaded properly
+    first_image = cv2.cvtColor(batch_images[0].numpy(), cv2.COLOR_BGR2RGB)
+    plt.imshow(first_image)
+    plt.show()
 
 # tests to check if the images are loaded properly
-
 """
+img_info_right = (images[0] * 255).astype('uint8')
+right_rgb = cv2.cvtColor(img_info_right, cv2.COLOR_BGR2RGB)
+
+plt.imshow(right_rgb)
+plt.show()
+
 img_info_right = (images_right[0] * 255).astype('uint8')
 right_rgb = cv2.cvtColor(img_info_right, cv2.COLOR_BGR2RGB)
 
@@ -55,3 +73,4 @@ axes[1].imshow(right_rgb)
 
 plt.show()
 """
+# tests to check that the arrays were filled correctly
